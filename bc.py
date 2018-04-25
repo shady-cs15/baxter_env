b252a924b3d67bbe391e163221db2a2cb72617aa
Satyaki Chakraborty <satyaki.cs15@gmail.com>
2018-04-25 17:02:30 -0400

    bc loss

--- bc.py
+++ bc.py
@@ -1,19 +1,59 @@
+from rllab.misc import ext
+
+### Adding an extra buffer for demos
+
+'''Create SimpleReplayPool instance
+'''
+# demo_buffer = SimpleReplayPool(max_pool_size=.., 
+#	observation_dim = ..,
+#	action_dim = ..)
+
+'''adding samples
+'''
+# demo_buffer.add_sample(observation, action, reward, terminal)
+
+'''getting random batch from demo_buffer
+'''
+# demo_batch = demo_buffer.random_batch(batch_size)
 
 
-# demo_batch is obtained from SimpleRelayPool.random_batch
-def bc_loss(demo_batch, Q, pi):
-	observations = demo_batch['observations']
-	actions = demo_batch['actions']
-	assert actions.shape[0] == observations.shape[0]
 
-	# actions : batch_size x action_dim
-	# observations : batch_size x observ_dim
+### Changes to make inside ddpg.py DDPG.init_opt()
+
+'''create bcloss line:286
+'''
+demo_obs = self.env.observation_space.new_tensor_variable('demo_obs', extra_dims=1)
+demo_actions = self.env.observation_space.new_tensor_variable('demo_actions', extra_dims=1)
+a_ = self.policy.get_action_sym(demo_obs)
+mask = self.qf.get_qval_sym(demo_obs, demo_actions) > self.qf.get_qval_sym(demo_obs, a_, deterministic=True)
+bcloss = TT.mean(TT.sum((a - a_)**2.0, axis=1)*mask)
+
+
+'''subtract bcloss to exisiting policy loss line:303 (check once)
+'''
+policy_loss = ...
+
+
+'''Change f_train_policy line:318
+'''
+f_train_policy = ext.compile_function(
+	inputs=[obs, demo_obs, demo_actions],
+	outputs=policy_loss,
+	updates=policy_updates
+	)
 	
-	loss = 0.
-	for i in len(range(actions.shape[0])):
-		ai = actions[i]
-		si = observations[i]
-		mask = 0.
-		if Q(si, ai) > Q(si, pi(si)):	mask = 1.
-		loss = loss + mask*(pi(si)-ai)**2.0
-	return loss
+
+
+### Changes to make inside ddpg.py DDPG.do_training()
+'''Get demonstrations from demo_batch line:338
+'''
+demo_obs, demo_actions, _, _, _ = ext.extract(
+            demo_batch,
+            "observations", "actions", "rewards", "next_observations",
+            "terminals"
+        )
+
+
+'''update inputs in f_train_policy line:353
+'''
+policy_surr = f_train_policy(obs, demo_obs, demo_actions)
